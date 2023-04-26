cfe529759c4c92ff164c1330c3b0cce567d3dedc
Chris Merritt <102829558+Chrismerritt91@users.noreply.github.com>
2023-04-26 16:30:50 -0500

    continue lessons on inheritance

--- Program.cs
+++ Program.cs
@@ -5,37 +5,29 @@
     {
 
         //create object of employee
-        Employee emp1 = new Employee();
-        emp1.EmpID = 101;
-        emp1.EmpName = "Scott";
-        emp1.Location = "Hyderabad";
+        Employee emp1 = new Employee(101, "Scott", "Hyderabad");
         Console.WriteLine("Object of Parent class (Employee):");
         Console.WriteLine(emp1.EmpID);
         Console.WriteLine(emp1.EmpName);
         Console.WriteLine(emp1.Location);
+        Console.WriteLine(emp1.GetHealthInsuranceAmount());
         Console.WriteLine();
 
 
         //create object of manager
-        Manager mgr1 = new Manager();
-        mgr1.EmpID = 102;
-        mgr1.EmpName = "Allen";
-        mgr1.Location = "New York";
-        mgr1.Department = "Accounting";
+        Manager mgr1 = new Manager(102, "Allen", "New York", "Accounting");
         Console.WriteLine("Object of Child class (Manager):");
         Console.WriteLine(mgr1.EmpID);
         Console.WriteLine(mgr1.EmpName);
         Console.WriteLine(mgr1.Location);
         Console.WriteLine(mgr1.Department);
         Console.WriteLine(mgr1.GetTotalSalesOfTheYear());
+        Console.WriteLine(mgr1.GetFullDepartmentName());
+        Console.WriteLine(mgr1.GetHealthInsuranceAmount());
         Console.WriteLine();
 
         //create object of salesman
-        Salesman salesman1 = new Salesman();
-        salesman1.EmpID = 101;
-        salesman1.EmpName = "John";
-        salesman1.Location = "Washington";
-        salesman1.Region = "East";
+        Salesman salesman1 = new Salesman(103,"John","Washington","East");
         Console.WriteLine("Object of Child class (Salesman):");
         Console.WriteLine(salesman1.EmpID);
         Console.WriteLine(salesman1.EmpName);
